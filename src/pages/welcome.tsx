bf63ba25f884cbbd07ced08d93f3290b286275f4
Deco Oliveira <deco.oliveira@googlemail.com>
2021-04-25 23:46:23 -0300

    README file added

---
+++ src/pages/welcome.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Text, View, Image, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
+import wateringImg from '../assets/watering.png';
+import colors from '../styles/colors';
+export function Welcome(){
+    return(
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Gerencie {'\n'}suas plantas de forma fácil</Text>
+            <Image source={wateringImg} style={styles.image}/>
+            <Text style={styles.subtitle    }>
+                Não esqueça de regar suas plantas. 
+                Nós cuidamos de lembrar você sempre que precisar
+            </Text>
+            <TouchableOpacity style={styles.button}>
+                <Text>
+                    D
+                </Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+        
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems: 'center',
+        justifyContent:'space-between'
+
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color:colors.heading,
+        marginTop: 30
+    },
+    subtitle: {
+        textAlign:'center',
+        fontSize:10,
+        paddingHorizontal: 20,
+        color: colors.heading
+    },
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56
+    },
+    image:{
+        width: 292,
+        height: 284
+    }
+});
